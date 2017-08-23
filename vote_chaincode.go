f1208f8ecce587bef1a002ab4c236375383ecbe9
Aviraj Khare <avirajkhare00@gmail.com>
2017-08-23 19:00:07 +0530

    chaincode working

--- vote_chaincode.go
+++ vote_chaincode.go
@@ -1,34 +1,22 @@
 /*
-Crowd Fund Chaincode
+esya Chaincode
 --------------------
 
-The purpose of this chaincode is to demonstrate a simple crowd funding
-application implemented on top of the Hyperledger Fabric platform. The
-application will expose an interface, where the end user may donate a certain
-amount of monetary units into an account. The account state variable will
-store the value of the total number of units donated.
+Right now, consider it as simple key-value store like redis :)
 
 The chaincode must implement the three methods required by the Fabric chaincode
 API: (1) Init, (2) Invoke, and (3) Query. In this example, the functionality of
 each of these methods is described below.
 
 (1) Init:
-	The method that is triggered when the chaincode is deployed. In this
-	example, the method initializes one state variable, "account", which will
-	store the total number of monetary units donated. The value is initialized
-	to the ammount denoted within the deployment request.
+	The method that is triggered when the chaincode is deployed.
 
 (2) Invoke:
 	The method that is triggered when a chaincode receives an invocation
-	transaction. In this example, the "account" variable will be increased by
-	the number of monetary units denoted within the request. As this method
-	modifies a state variable, it will be recoded as a transaction on the ledger.
+	transaction.
 
 (3) Query:
 	The method that is triggered when a chaincode receives a query transaction.
-	In this example, the "account" variable will be retrieved from the ledger
-	and its value will be returned to the client in the response. As this method
-	does not modify any state variable, this will not be recorded on the ledger.
 
 The chaincode must also contain the main() method, which starts the chaincode
 when it is first deployed.
@@ -39,7 +27,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
